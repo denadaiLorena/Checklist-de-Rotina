5885a8513bc037ee6ee617cc04ad659e7eedcffa
Lorena <lorenadenadai679@gmail.com>
2026-02-28 18:09:49 -0300

    adicionei o banco de dados na aplicação

--- backend/database/connection.php
+++ backend/database/connection.php
@@ -1,12 +1,14 @@
 <?php
 
     $hostname = "aws-1-us-east-1.pooler.supabase.com";
-    $dbname = "checklist_rotina";
+    $dbname = "postgres";
+    $port = 5432;
     $username = "postgres.ldozdavubrsehsxeaofw";
     $password = "6794check2026";
 
     try {
-        $pdo = new PDO("pgsql:host=$hostname;dbname=$dbname, $username,  $password");
+        $dsn = "pgsql:host=$hostname;port=$port;dbname=$dbname;sslmode=require";
+        $pdo = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
-        echo ("Erro ao conectar ao banco de dados: " . $e->getMessage());
+        echo ("Erro ao conectar ao banco: " . $e->getMessage());
     }
